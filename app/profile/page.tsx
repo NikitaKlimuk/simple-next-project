6dd2fc58ef4e87dae0d1ad6665ebe2e53f0f0cd8
Nikita Klimuk <nikitaklimukwot@gmail.com>
2023-11-28 21:26:49 +0100

    fix

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,10 @@
-import { auth, signIn } from "@/configs/auth";
+import { auth } from "@/configs/auth";
 
 export default async function Profile() {
   const session = await auth();
   return (
     <div>
       <h1>Profile of {session?.user?.name}</h1>
-      <form
-        action={async () => {
-          "use server";
-          await signIn("google");
-        }}
-      >
-        <button className="border p-4">Sign in with Google</button>
-      </form>
     </div>
   );
 }
